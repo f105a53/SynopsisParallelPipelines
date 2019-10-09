984885d11461e80ebbf22f3fdc51d47ce3095afa
J2ghz <j2.00ghz@gmail.com>
2019-10-09 12:51:09 +0200

    Add project files.

---
+++ SynopsisParallelPipelines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SynopsisParallelPipelines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
